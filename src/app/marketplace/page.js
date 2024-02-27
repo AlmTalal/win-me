1490f437d6e1c48b5bbc0b3c074c716c4e787dc6
Talal <alamtalal004@gmail.com>
2024-02-27 14:42:49 -0700

    Added the first version of the marketplace

---
+++ src/app/marketplace/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MarketPlace() {
+  return (
+    <div className="col-span-5 border-2 border-red-700 flex h-[85vh] rounded-lg justify-center items-center">
+      <p>Coming soon</p>
+    </div>
+  );
+}
