ba6baf7fd5b35d0225638c62bac3f0824b69c06a
Talal <alamtalal004@gmail.com>
2024-02-27 09:03:27 -0700

    Added shadcn UI library

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    THIRD_WEB_CLIENT_ID: "83f1d38b16b67f1f299a3f630bc54bb2",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
