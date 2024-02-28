f5b529d643df7b82cc55d56176e44778342c368c
Talal <alamtalal004@gmail.com>
2024-02-28 14:40:12 -0700

    removed the third web sdk

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    THIRD_WEB_CLIENT_ID: "83f1d38b16b67f1f299a3f630bc54bb2",
-  },
   images: {
     remotePatterns: [
       {
