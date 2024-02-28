92d460f8aa3e89c43811e59ab20f1a6f0cc59be3
Talal <alamtalal004@gmail.com>
2024-02-28 12:10:31 -0700

    Changed the description od the page

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Win-me",
-  description: "Let's race!",
+  description: "Let's win some money racing !",
 };
 
 export default function RootLayout({ children }) {
