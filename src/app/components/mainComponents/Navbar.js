a3c5e11c0ef480c81cc580e4a69da2d491ca2348
Talal <alamtalal004@gmail.com>
2024-02-27 19:55:21 -0700

    Added the authorization for going to gaming page

--- src/app/components/mainComponents/Navbar.js
+++ src/app/components/mainComponents/Navbar.js
@@ -1,24 +1,52 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { PlayIcon } from "@radix-ui/react-icons";
 import { ShoppingCartIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import routes from "@/routes";
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export default function Navbar() {
+  const address = useAddress();
+
+  useEffect(() => {
+    if (address !== undefined) {
+      setShowDialog(false);
+    }
+  }, [address]);
+
   const router = useRouter();
+  const [showDialog, setShowDialog] = useState(false);
 
   const reRouteUser = (page) => {
     router.push(page);
+  };
+
+  const reRouteToGamingPage = (page) => {
+    if (address !== undefined) {
+      router.push(page);
+      return;
+    }
+
+    setShowDialog(!showDialog);
   };
 
   return (
     <div className=" flex flex-1 flex-col justify-evenly items-center w-full h-full rounded-lg border-2 border-red-700">
       <Button
         className="my-10 p-5 w-40"
-        onClick={() => reRouteUser(routes.gaming)}
+        onClick={() => reRouteToGamingPage(routes.gaming)}
       >
         <PlayIcon />
         <p className="ml-2">Let's play!</p>
@@ -30,6 +58,24 @@ export default function Navbar() {
         <ShoppingCartIcon />
         <p className="ml-2">Let's shop!</p>
       </Button>
+      <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
+        <AlertDialogContent className="text-white bg-black border-red-600">
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Hey, we need you to connect your Metamask
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              We promise that we will do our best efforts to not to scam you 🤣
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="p-4 px-7 w-auto h-auto text-black font-semibold font-xl">
+              Cancel
+            </AlertDialogCancel>
+            <ConnectWallet />
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
