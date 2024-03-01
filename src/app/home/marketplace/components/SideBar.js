7d1c2710a24bd5260283ee49a2e2daa3a801715c
Talal <alamtalal004@gmail.com>
2024-03-01 16:42:21 -0700

    Added the input field for buying the WinMe token

--- src/app/home/marketplace/components/SideBar.js
+++ src/app/home/marketplace/components/SideBar.js
@@ -2,16 +2,35 @@
 
 import { Button } from "@/components/ui/button";
 import routes from "@/routes";
-import { ShoppingBagIcon, BadgeCheckIcon } from "lucide-react";
+import { ShoppingBagIcon, BadgeCheckIcon, XIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { Input } from "@/components/ui/input";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function SideBar() {
+  const [showDialog, setShowDialog] = useState(false);
   const router = useRouter();
 
   const routeUser = (path) => {
     router.push(path);
+  };
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+    const amountToBuy = Object.fromEntries(data.entries()).tokensToBuy;
+    console.log(amountToBuy);
+    setShowDialog(false);
   };
 
   const path = usePathname();
@@ -25,7 +44,7 @@ export default function SideBar() {
         onClick={() => routeUser(routes.marketplace)}
       >
         <ShoppingBagIcon className="mr-2 h-4 w-4" />
-        Market Place
+        Marketplace
       </Button>
       <Button
         className={` hover:bg-white hover:text-black px-7 py-5 my-4 ${
@@ -36,6 +55,43 @@ export default function SideBar() {
         <BadgeCheckIcon className="mr-2 h-4 w-4" />
         Your Nfts
       </Button>
+      <Button
+        className={"hover:bg-white hover:text-black px-7 py-5 my-4 text-white "}
+        onClick={() => setShowDialog(true)}
+      >
+        Buy Win-Me Tokens
+      </Button>
+      <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
+        <AlertDialogContent className="text-white bg-black border-red-600">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="flex justify-between items-center ">
+              Let's buy some Win-Me Tokens
+              <AlertDialogCancel className="h-auto w-auto bg-black border-black hover:bg-black hover:text-white">
+                <XIcon />
+              </AlertDialogCancel>
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              <div className="flex w-full max-w-sm items-center space-x-2">
+                <form
+                  onSubmit={(e) => handleForm(e)}
+                  className="flex w-full justify-between items-center "
+                >
+                  <Input
+                    type="number"
+                    min="1"
+                    placeholder="Amount to buy"
+                    className="text-white"
+                    name="tokensToBuy"
+                  />
+                  <Button type="submit" className="mx-5">
+                    Buy
+                  </Button>
+                </form>
+              </div>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
